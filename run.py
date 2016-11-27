069a4f873c4ad77c6d896b4a56bccd957c1a19ab
Tyan Boot <tyan-boot@outlook.com>
2016-11-27 18:05:01 +0800

    定时任务完善，模态框已可以正常使用 引入Foundation-CSS
    Signed-off-by: Tyan Boot <tyan-boot@outlook.com>

--- run.py
+++ run.py
@@ -127,4 +127,4 @@ def NotFound(error):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
--- utils/lib_db.py
+++ utils/lib_db.py
@@ -8,15 +8,21 @@ def init_db():
           `subid` INTEGER PRIMARY KEY AUTOINCREMENT ,
           `user` VARCHAR(64) NOT NULL DEFAULT '0',
           `subscription` VARCHAR(12) DEFAULT '0',
+          `book_name` VARCHAR(64),
           `is_del` TINYINT(4) DEFAULT '0'
         )""")
-
-    db_con.commit()
 
     cur.execute("""CREATE TABLE IF NOT EXISTS `user` (
           `uid` INTEGER PRIMARY KEY AUTOINCREMENT ,
           `user` VARCHAR(64) NOT NULL DEFAULT '0',
           `email` VARCHAR(128) NOT NULL DEFAULT '0'
+        )""")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS `details` (
+        `did` INTEGER PRIMARY KEY AUTOINCREMENT ,
+        `book_id` VARCHAR(12),
+        `book_name` VARCHAR(64),
+        `available` INTEGER
         )""")
 
     db_con.commit()
